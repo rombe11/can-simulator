fa038ba95541ac8a39c7a5951916a358832c75bc
Rombu11 <rombe1@ac.sce.ac.il>
2026-08-20 13:15:27 +0300

    use new display monitor

--- can_simulator/simulation/poc.py
+++ can_simulator/simulation/poc.py
@@ -1,7 +1,7 @@
 import time
 
 from can_simulator.core.bus import BusConfig, CanBus
-from can_simulator.core.monitor import BusMonitor
+from can_simulator.core.monitor import BusMonitor, PCANStyleMonitor
 from can_simulator.nodes.dumb_node import DumbNode
 
 
@@ -24,15 +24,17 @@ def main() -> None:
         )
     )
 
-    can0.add_listener(BusMonitor("CAN0"))
-    can1.add_listener(BusMonitor("CAN1"))
-
+    monitor = PCANStyleMonitor("GLOBAL_MONITOR", update_interval=0.1)
+    
+    can0.add_listener(monitor)
+    can1.add_listener(monitor)
+    
     node1 = DumbNode(
         name="NODE_1",
         bus=can0,
         arbitration_id=0x100,
         period_s=0.1,
-        data=b"\x01\x02\x03\x04\x05\x06\x07\x08",
+        data=bytearray(b"\x01\x02\x03\x04\x05\x06\x07\x08"),
     )
 
     node2 = DumbNode(
@@ -40,7 +42,7 @@ def main() -> None:
         bus=can0,
         arbitration_id=0x200,
         period_s=0.5,
-        data=b"\x10\x20\x30\x40\x50\x60\x70\x80",
+        data=bytearray(b"\x10\x20\x30\x40\x50\x60\x70\x80"),
     )
 
     node3 = DumbNode(
@@ -48,7 +50,7 @@ def main() -> None:
         bus=can1,
         arbitration_id=0x300,
         period_s=0.2,
-        data=b"\xAA\xBB\xCC\xDD\xEE\xFF\x00\x11",
+        data=bytearray(b"\xAA\xBB\xCC\xDD\xEE\xFF\x00\x11"),
     )
 
     node4 = DumbNode(
@@ -56,7 +58,7 @@ def main() -> None:
         bus=can1,
         arbitration_id=0x400,
         period_s=1.0,
-        data=b"\x11\x22\x33\x44\x55\x66\x77\x88",
+        data=bytearray(b"\x11\x22\x33\x44\x55\x66\x77\x88"),
     )
 
     nodes = [
@@ -72,22 +74,36 @@ def main() -> None:
     for node in nodes:
         node.start()
 
-    print("CAN simulator running")
+    print("CAN simulator running (PCAN Style with Dynamic Data)")
     print("Press Ctrl+C to stop")
+
+    counter = 0
 
     try:
         while True:
             time.sleep(1)
+            counter = (counter + 1) % 256
+            
+            node1.data[0] = counter
+            node3.data[0] = (counter * 2) % 256
 
     except KeyboardInterrupt:
-        print("\nStopping...")
+        print("\nStopping simulation gracefully...")
 
     finally:
         for node in nodes:
-            node.stop()
+            try:
+                node.stop()
+            except Exception:
+                pass
 
-        can0.shutdown()
-        can1.shutdown()
+        for bus in [can0, can1]:
+            try:
+                bus.shutdown()
+            except Exception:
+                pass
+        
+        print("Simulator stopped successfully.")
 
 
 if __name__ == "__main__":
