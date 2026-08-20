20eff1a05fa736d03d9af6f0f867fec40e05ad77
Rombu11 <rombe1@ac.sce.ac.il>
2026-08-20 13:32:36 +0300

    poc improved: use senders & updating msg values dynamically

--- can_simulator/simulation/poc.py
+++ can_simulator/simulation/poc.py
@@ -1,14 +1,14 @@
 import time
 
 from can_simulator.core.bus import BusConfig, CanBus
-from can_simulator.core.monitor import BusMonitor, PCANStyleMonitor
-from can_simulator.nodes.dumb_node import DumbNode
+from can_simulator.core.monitor import PCANStyleMonitor
+from can_simulator.core.sender import PeriodicSender
 
 
 def main() -> None:
     can0 = CanBus(
         BusConfig(
-            channel="poc_can0",
+            channel="CAN0",
             interface="virtual",
             bitrate=500_000,
             receive_own_messages=True,
@@ -17,7 +17,7 @@ def main() -> None:
 
     can1 = CanBus(
         BusConfig(
-            channel="poc_can1",
+            channel="CAN1",
             interface="virtual",
             bitrate=500_000,
             receive_own_messages=True,
@@ -28,74 +28,39 @@ def main() -> None:
     
     can0.add_listener(monitor)
     can1.add_listener(monitor)
-    
-    node1 = DumbNode(
-        name="NODE_1",
-        bus=can0,
-        arbitration_id=0x100,
-        period_s=0.1,
-        data=bytearray(b"\x01\x02\x03\x04\x05\x06\x07\x08"),
-    )
 
-    node2 = DumbNode(
-        name="NODE_2",
-        bus=can0,
-        arbitration_id=0x200,
-        period_s=0.5,
-        data=bytearray(b"\x10\x20\x30\x40\x50\x60\x70\x80"),
-    )
+    sender0 = PeriodicSender("Sender_CAN0", can0)
+    sender1 = PeriodicSender("Sender_CAN1", can1)
 
-    node3 = DumbNode(
-        name="NODE_3",
-        bus=can1,
-        arbitration_id=0x300,
-        period_s=0.2,
-        data=bytearray(b"\xAA\xBB\xCC\xDD\xEE\xFF\x00\x11"),
-    )
+    sender0.add_raw("status_node1", arbitration_id=0x100, period_s=0.1, data=b"\x01\x02\x03\x04\x05\x06\x07\x08")
+    sender0.add_raw("status_node2", arbitration_id=0x200, period_s=0.5, data=b"\x10\x20\x30\x40\x50\x60\x70\x80")
 
-    node4 = DumbNode(
-        name="NODE_4",
-        bus=can1,
-        arbitration_id=0x400,
-        period_s=1.0,
-        data=bytearray(b"\x11\x22\x33\x44\x55\x66\x77\x88"),
-    )
+    sender1.add_raw("status_node3", arbitration_id=0x300, period_s=0.2, data=b"\xAA\xBB\xCC\xDD\xEE\xFF\x00\x11")
+    sender1.add_raw("status_node4", arbitration_id=0x400, period_s=1.0, data=b"\x11\x22\x33\x44\x55\x66\x77\x88")
 
-    nodes = [
-        node1,
-        node2,
-        node3,
-        node4,
-    ]
+    sender0.start()
+    sender1.start()
 
-    for node in nodes:
-        node.initialize()
-
-    for node in nodes:
-        node.start()
-
-    print("CAN simulator running (PCAN Style with Dynamic Data)")
+    print("CAN simulator running (Unified PCAN Style Monitor with Periodic Dynamic Updates)")
     print("Press Ctrl+C to stop")
 
     counter = 0
 
     try:
         while True:
-            time.sleep(1)
+            time.sleep(2.0)
             counter = (counter + 1) % 256
-            
-            node1.data[0] = counter
-            node3.data[0] = (counter * 2) % 256
+
+            sender0.set_byte("status_node1", index=0, value=counter)
+            sender0.set_byte("status_node2", index=1, value=(counter * 2) % 256)
+            sender1.set_byte("status_node3", index=7, value=255 - counter)
 
     except KeyboardInterrupt:
         print("\nStopping simulation gracefully...")
 
     finally:
-        for node in nodes:
-            try:
-                node.stop()
-            except Exception:
-                pass
+        sender0.stop()
+        sender1.stop()
 
         for bus in [can0, can1]:
             try:
